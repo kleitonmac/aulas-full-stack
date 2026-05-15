2def0833ec80fc738eb0aa37299badca880c555d
Kleiton Santos Macedo <146167761+kleitonmac@users.noreply.github.com>
2026-05-15 01:36:50 -0300

    fix atualizacoes no home cores e mordernizacao

---
+++ cursos.javascript/objeto-literal/objetoLiteral.js
@@ -0,0 +1,16 @@
+const Pessoa = {
+    nome: 'Kleiton',
+    idade: 29,
+    genero: 'masculino',
+    altura: 1.75,
+    cpf: '12345678900',
+    endereco: {
+        rua: 'Uberlândia',
+        numero: '123',
+        bairro: 'Centro',
+        cidade: 'Vila Velha',
+        estado: 'ES',
+        cep: '29100-000'
+    }
+
+}
